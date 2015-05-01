e9937e9cd6427af86d9c9b3611b79adc0019e9d2
shameful <kick.b0rt@gmail.com>
2015-05-01 00:10:54 -0500

    initial commit

---
+++ src/Irc/Driver.java
@@ -0,0 +1,77 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+
+
+
+import java.awt.Dimension;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.JScrollPane;
+import javax.swing.JTextArea;
+import javax.swing.JTextField;
+
+/**
+ *
+ * 
+ */
+public class Driver
+{
+    
+    private static IOHandler client;
+    private static JTextField inputfield;
+    private static JTextArea outputfield;
+    
+    public static void main(String[] args)
+    {
+        JFrame window = new JFrame("window");
+        window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        JPanel panel = new JPanel();
+        panel.setPreferredSize(new Dimension(900,675));
+        
+        inputfield = new JTextField(80);
+        inputfield.addActionListener(new Driver().new UserInputAction());
+        
+        outputfield = new JTextArea(40, 80);
+        outputfield.setEditable(false);
+        outputfield.setLineWrap(true);
+        
+        JScrollPane scroller = new JScrollPane(outputfield);
+        
+        window.add(panel);
+        panel.add(scroller);
+        panel.add(inputfield);
+        window.pack();
+        window.setVisible(true);
+        
+        client = new IOHandler("irc.synirc.net", 6667);
+        client.setReadListener(new Driver().new ClientOutputAction());
+    }
+    
+    private class UserInputAction implements ActionListener
+    {
+
+        @Override public void actionPerformed(ActionEvent ae)
+        {
+            client.writeToSocket(inputfield.getText());
+            outputfield.append(inputfield.getText() + "\n");
+            inputfield.setText("");
+        }
+        
+    }
+    
+    private class ClientOutputAction implements ActionListener
+    {
+
+        @Override public void actionPerformed(ActionEvent ae)
+        {
+            outputfield.append(ae.getActionCommand() + "\n");
+        }
+        
+    }
+}
---
+++ src/Irc/IOHandler.java
@@ -0,0 +1,101 @@
+/*
+ * To change this license header, choose License Headers socketInWrapper Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template socketInWrapper the editor.
+ */
+
+
+
+
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.io.DataOutputStream;
+import java.io.IOException;
+import java.net.Socket;
+import java.net.UnknownHostException;
+import java.util.Scanner;
+import javax.swing.SwingUtilities;
+
+/**
+ *
+ * 
+ */
+public class IOHandler
+{
+    
+    private Socket sock;
+    private DataOutputStream outStream;
+    private Scanner socketInWrapper;
+    private ActionListener readListener;
+    private String streamInput;
+    private Thread readerThread;
+    
+    
+    public IOHandler(String server, int port)
+    {
+        try
+        {
+            sock = new Socket(server, port);
+            outStream = new DataOutputStream(sock.getOutputStream());
+            socketInWrapper = new Scanner(sock.getInputStream());
+            readerThread = new Thread(new ReadThreaded());
+            readerThread.setDaemon(true);
+            readerThread.start();
+        }
+        catch(UnknownHostException exc) {stringOutClient("Unknown host: " + server);}
+        catch(IOException exc) {stringOutClient("IO exception during setup");}
+    }
+    
+    public void writeToSocket(String str)
+    {
+        if (outStream == null){stringOutClient("err: no connection");return;}
+        try{outStream.writeBytes(str + "\r\n");} catch(IOException exc) {stringOutClient("err: write failed: " + str);}
+    }
+    
+    private void stringOutClient(String str)
+    {
+        readListener.actionPerformed(new ActionEvent(this, 0, str));
+    }
+    
+    private void pingReply(String str)
+    {
+        writeToSocket("PONG " + str.substring(5));
+    }
+    
+    public void setReadListener(ActionListener al)
+    {
+        readListener = al;
+    }
+    
+    
+    private class swingInvokerForThread implements Runnable
+    {
+        private String str;
+        
+        public swingInvokerForThread(String toDisplay)
+        {
+            str = toDisplay;
+        }
+        
+        @Override public void run()
+        {
+            stringOutClient(str);
+        }
+    }
+    
+    private class ReadThreaded implements Runnable
+    {
+        @Override public void run()
+        {
+            while(true)
+            {
+                if (socketInWrapper.hasNextLine())
+                {
+                    streamInput = socketInWrapper.nextLine();
+                    if(streamInput.startsWith("PING ")) {pingReply(streamInput);}
+                    else {SwingUtilities.invokeLater(new swingInvokerForThread(streamInput));}
+                }
+            }
+        }
+    }
+}
