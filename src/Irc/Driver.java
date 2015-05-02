f97fc98f524ba0afc6dfffb72eedfa1c29452f48
shameful <kick.b0rt@gmail.com>
2015-05-02 18:37:54 -0500

    changed cursor behavior and colors
    cursor is now always visible in outputfield. foreground/background
    colors are now yellow/black

--- src/Irc/Driver.java
+++ src/Irc/Driver.java
@@ -7,9 +7,12 @@
 
 
 
+import java.awt.Color;
 import java.awt.Dimension;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.awt.event.FocusEvent;
+import java.awt.event.FocusListener;
 import javax.swing.JFrame;
 import javax.swing.JOptionPane;
 import javax.swing.JPanel;
@@ -25,24 +28,44 @@ import javax.swing.text.DefaultCaret;
 public class Driver
 {
     
+    public static final Color FOREGROUND_COLOR = new Color(0xEA, 0xCF, 0x4C);
+    public static final Color BACKGROUND_COLOR = new Color(0x00, 0x00, 0x00);
     private static IOHandler client;
     private static JTextField inputfield;
     private static JTextArea outputfield;
     
     public static void main(String[] args)
     {
-        JFrame window = new JFrame("window");
+        JFrame window = new JFrame("IRC");
         window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         JPanel panel = new JPanel();
         panel.setPreferredSize(new Dimension(900,675));
+        panel.setForeground(FOREGROUND_COLOR);
+        panel.setBackground(BACKGROUND_COLOR);
         
         inputfield = new JTextField(80);
         inputfield.addActionListener(new Driver().new UserInputAction());
+        inputfield.setForeground(FOREGROUND_COLOR);
+        inputfield.setBackground(BACKGROUND_COLOR);
+        inputfield.setCaretColor(FOREGROUND_COLOR);
         
         outputfield = new JTextArea(40, 80);
         outputfield.setEditable(false);
         outputfield.setLineWrap(true);
+        outputfield.setForeground(FOREGROUND_COLOR);
+        outputfield.setBackground(BACKGROUND_COLOR);
         ((DefaultCaret)outputfield.getCaret()).setUpdatePolicy(DefaultCaret.ALWAYS_UPDATE);
+        outputfield.getCaret().setVisible(true);
+        outputfield.setCaretColor(FOREGROUND_COLOR);
+        outputfield.addFocusListener(new 
+            FocusListener()
+            {
+
+                @Override public void focusGained(FocusEvent fe) {}
+
+                @Override public void focusLost(FocusEvent fe)
+                {outputfield.getCaret().setVisible(true);}
+            });
         
         JScrollPane scroller = new JScrollPane(outputfield);
         
