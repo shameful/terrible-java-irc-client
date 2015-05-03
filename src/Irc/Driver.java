ec9108842dd03e8e1856d621d2773109f3d88ced
shameful <kick.b0rt@gmail.com>
2015-05-03 16:24:14 -0500

    cleaned up unused imports in Driver

--- src/Irc/Driver.java
+++ src/Irc/Driver.java
@@ -7,19 +7,11 @@
 
 
 
-import java.awt.Color;
-import java.awt.Dimension;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
-import java.awt.event.FocusEvent;
-import java.awt.event.FocusListener;
-import javax.swing.JFrame;
 import javax.swing.JOptionPane;
-import javax.swing.JPanel;
-import javax.swing.JScrollPane;
 import javax.swing.JTextArea;
 import javax.swing.JTextField;
-import javax.swing.text.DefaultCaret;
 
 /**
  *
@@ -28,8 +20,6 @@ import javax.swing.text.DefaultCaret;
 public class Driver
 {
     
-    public static final Color FOREGROUND_COLOR = new Color(0xEA, 0xCF, 0x4C);
-    public static final Color BACKGROUND_COLOR = new Color(0x00, 0x00, 0x00);
     private static IOHandler client;
     private static JTextField inputfield;
     private static JTextArea outputfield;
