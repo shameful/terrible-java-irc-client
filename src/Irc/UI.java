71e2cf3482f33706a01261f98590a21ceda4cb12
shameful <kick.b0rt@gmail.com>
2015-05-03 15:36:35 -0500

    Added constructor for UI.java

--- src/Irc/UI.java
+++ src/Irc/UI.java
@@ -3,10 +3,12 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-
 package Irc;
 
+import java.awt.*;
+import java.awt.event.*;
 import javax.swing.*;
+import javax.swing.text.DefaultCaret;
 
 /**
  *
@@ -14,9 +16,54 @@ import javax.swing.*;
  */
 public class UI
 {
+
+    public static final Color FOREGROUND_COLOR_DEFAULT = new Color(0xEA, 0xCF, 0x4C);
+    public static final Color BACKGROUND_COLOR_DEFAULT = new Color(0x00, 0x00, 0x00);
     private JFrame window;
     private JPanel panel;
     private JScrollPane scroller;
     private JTextArea output;
     private JTextField input;
+
+    public UI()
+    {
+        window = new JFrame("IRC");
+        panel = new JPanel();
+        input = new JTextField(80);
+        output = new JTextArea(40, 80);
+        scroller = new JScrollPane(output);
+        
+        window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        
+        panel.setPreferredSize(new Dimension(900, 675));
+        panel.setForeground(FOREGROUND_COLOR_DEFAULT);
+        panel.setBackground(BACKGROUND_COLOR_DEFAULT);
+        
+        input.setForeground(FOREGROUND_COLOR_DEFAULT);
+        input.setBackground(BACKGROUND_COLOR_DEFAULT);
+        input.setCaretColor(FOREGROUND_COLOR_DEFAULT);
+        
+        output.setEditable(false);
+        output.setLineWrap(true);
+        output.setForeground(FOREGROUND_COLOR_DEFAULT);
+        output.setBackground(BACKGROUND_COLOR_DEFAULT);
+        ((DefaultCaret) output.getCaret()).setUpdatePolicy(DefaultCaret.ALWAYS_UPDATE);
+        output.getCaret().setVisible(true);
+        output.setCaretColor(FOREGROUND_COLOR_DEFAULT);
+        output.addFocusListener(new 
+            FocusListener()
+            {
+
+                @Override public void focusGained(FocusEvent fe) {}
+
+                @Override public void focusLost(FocusEvent fe)
+                    {output.getCaret().setVisible(true);}
+            });
+        
+        window.add(panel);
+        panel.add(scroller);
+        panel.add(input);
+        window.pack();
+        window.setVisible(true);
+    }
 }
