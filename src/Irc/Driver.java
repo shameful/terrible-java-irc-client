e7b1a2f51c5ebb562dd3af6885e5773348c7abc8
shameful <kick.b0rt@gmail.com>
2015-05-03 17:03:42 -0500

    changed variable "client" to local variable

--- src/Irc/Driver.java
+++ src/Irc/Driver.java
@@ -11,14 +11,12 @@
 public class Driver
 {
     
-    private static IOHandler client;
-    
     public static void main(String[] args)
     {
         UI testui = new UI();
         String serverHostname = testui.getUserInputBlocking("Enter server hostname");
         int portNumber = Integer.parseInt(testui.getUserInputBlocking("Enter port number\n (6667 is a common port number)"));
-        client = new IOHandler(serverHostname, portNumber);
+        IOHandler client = new IOHandler(serverHostname, portNumber);
         client.setReadListener(testui.getOutputListener());
         testui.addInputListener(client.getWriteListener());
     }
